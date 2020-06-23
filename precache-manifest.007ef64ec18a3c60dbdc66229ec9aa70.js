e43e47b29042877f799711fdc42a27a13f063db7
Rever <rever22411@gmail.com>
2020-06-23 14:00:05 +0200

    Updates

---
+++ precache-manifest.007ef64ec18a3c60dbdc66229ec9aa70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11ba996873380247fa5d1783f1c2177f",
+    "url": "/configuratore-len/index.html"
+  },
+  {
+    "revision": "148d83504149f95ca845",
+    "url": "/configuratore-len/static/css/main.f5ec9c34.chunk.css"
+  },
+  {
+    "revision": "48ca44074752e5a5e7fe",
+    "url": "/configuratore-len/static/js/2.6ea8504e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/configuratore-len/static/js/2.6ea8504e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "148d83504149f95ca845",
+    "url": "/configuratore-len/static/js/main.61453efb.chunk.js"
+  },
+  {
+    "revision": "898a16f9a6cf4bf15bec",
+    "url": "/configuratore-len/static/js/runtime-main.b2d6faad.js"
+  }
+]);
